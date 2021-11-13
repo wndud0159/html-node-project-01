17795808b11c81e07cdf21cc4917ea705556ba8d
wndud0159 <wndud0159@naver.com>
2021-11-13 21:25:13 +0900

    modified

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+const onEmailSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value)
+    console.log(e.target[1].value)
+
+    axios.post('/create', {
+        name: e.target[0].value,
+        email: e.target[1].value,
+    }).then(response => {
+        if(response.data.error) {
+            console.log(response.data.message)
+            document.querySelector("#message").value = response.data.message
+            return
+        }
+        window.location.replace(`/waitinglist?id=${response.data.list.id}`);
+    }).catch(error => {
+        console.log("create error: ", error)
+    })
+}
