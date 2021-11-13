c1ce8362d9c4ab66cb407cd071a4093698e632a0
wndud0159 <wndud0159@naver.com>
2021-11-14 01:14:28 +0900

    modified

--- public/js/waitinglist.js
+++ public/js/waitinglist.js
@@ -20,48 +20,47 @@
         }
         getinfo();
 
-        function copy() {
-            var obj = document.getElementById("ibackURL");
-            var range = document.createRange();
-            range.selectNode(obj.childNodes[0]); //텍스트 정보를 Range 객체에 저장
-            var sel = window.getSelection();
-            sel.removeAllRanges(); //기존 선택정보 삭제
-            sel.addRange(range); //텍스트 정보 선택
-            document.execCommand("copy"); //복사
-            sel.removeRange(range); //선택 정보 삭제
+        // function copy() {
+        //     var obj = document.getElementById("ibackURL");
+        //     var range = document.createRange();
+        //     range.selectNode(obj.childNodes[0]); //텍스트 정보를 Range 객체에 저장
+        //     var sel = window.getSelection();
+        //     sel.removeAllRanges(); //기존 선택정보 삭제
+        //     sel.addRange(range); //텍스트 정보 선택
+        //     document.execCommand("copy"); //복사
+        //     sel.removeRange(range); //선택 정보 삭제
 
-            // document.querySelector("#message").value = 'Copied! 링크를 공유하세요!'
-            alert('복사 되었습니다. 친구에게 링크를 공유해보세요!')
-        }
-        
+        //     // document.querySelector("#message").value = 'Copied! 링크를 공유하세요!'
+        //     alert('복사 되었습니다. 친구에게 링크를 공유해보세요!')
+        // }
+
         Kakao.init('27c3bac20c362a640406590367df687d');
-        function kakaoShare() {
-            Kakao.Link.sendDefault({
-                objectType: 'feed',
-                content: {
-                title: '아이백',
-                description: '곧 출시될 아이백 서비스의 웨이팅 리스트에 등록하세요!',
-                imageUrl: 'https://waitinglist.iback.co/iback_main_image02.png',
+        Kakao.Link.createDefaultButton({
+            container: '#kakao_share',
+            objectType: 'feed',
+            content: {
+            title: '아이백',
+            description: '곧 출시될 아이백 서비스의 웨이팅 리스트에 등록하세요!',
+            imageUrl: 'https://waitinglist.iback.co/iback_main_image02.png',
+            link: {
+                mobileWebUrl: 'https://waitinglist.iback.co',
+                webUrl: 'https://waitinglist.iback.co',
+            },
+            },
+            buttons: [
+            {
+                title: '웹으로 보기',
                 link: {
-                    mobileWebUrl: 'https://waitinglist.iback.co',
-                    webUrl: 'https://waitinglist.iback.co',
+                mobileWebUrl: 'https://waitinglist.iback.co',
+                webUrl: 'https://waitinglist.iback.co',
                 },
-                },
-                buttons: [
-                {
-                    title: '웹으로 보기',
-                    link: {
-                    mobileWebUrl: 'https://waitinglist.iback.co',
-                    webUrl: 'https://waitinglist.iback.co',
-                    },
-                },
-                ],
-                // 카카오톡 미설치 시 카카오톡 설치 경로이동
-                installTalk: true,
-            })
-        }
+            },
+            ],
+        })
+   
+        
 
-const copyButton = document.getElementById('copyButton')
+// const copyButton = document.getElementById('copyButton')
 
-copyButton.addEventListener("click", kakaoShare)
+// copyButton.addEventListener("click", test)
         
